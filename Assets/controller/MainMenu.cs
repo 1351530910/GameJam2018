3f828fa244afa517955d5ad987a02ff13eeb0c4f
zyyangcnd <35665776+zyyangcnd@users.noreply.github.com>
2018-03-24 19:58:24 -0400

    Pause feature added

--- Assets/controller/MainMenu.cs
+++ Assets/controller/MainMenu.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
+	GameObject[] pauseObjects;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag ("SettingsPaused");
+		foreach (GameObject g in pauseObjects) {
+			g.SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void showSetting() {
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+			foreach (GameObject g in pauseObjects) {
+				g.SetActive (true);
+			}
+		} else {
+			Time.timeScale = 1;
+			foreach (GameObject g in pauseObjects) {
+				g.SetActive (false);
+			}
+		}
+	}
+
 
 	public void loadLevel(string level) {
 		Application.LoadLevel (level);
