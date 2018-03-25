ff41ca3b88d9415dc462dfb335efeb3d11f1072c
zyyangcnd <35665776+zyyangcnd@users.noreply.github.com>
2018-03-25 10:35:55 -0400

    Game Over Vomit Added

--- Assets/controller/ColletableScript.cs
+++ Assets/controller/ColletableScript.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColletableScript : MonoBehaviour {
-	
+
+
+
 	void Update () {
-		
-		transform.Translate(new Vector2(0f, 0.04f));  
 
-	if (transform.position.y> 5.0f){
-			this.gameObject.SetActive (false);
+		if (SceneManager.GetActiveScene ().name == "main") {
+
+			transform.Translate (new Vector2 (0f, 0.04f));  
+
+			if (transform.position.y > 5.0f) {
+
+				this.gameObject.SetActive (false);
+
+			}
 		}
-	}
 
- 
+	}
 }
