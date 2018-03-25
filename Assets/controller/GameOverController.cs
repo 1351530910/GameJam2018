46cc3c17253c0169caf35c58d3c587ce5109bdb7
zyyangcnd <35665776+zyyangcnd@users.noreply.github.com>
2018-03-25 12:55:20 -0400

    Fish Storm Vomit Added

--- Assets/controller/GameOverController.cs
+++ Assets/controller/GameOverController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class GameOverController : MonoBehaviour
 	public GameObject MedCat;
 	public GameObject SkinnyCat;
 	public GameObject Score;
+	public Text ScoreText;
+
 	public GameObject ReturnButton;
 	public GameObject SpeechBubble;
 
@@ -23,9 +26,10 @@ public class GameOverController : MonoBehaviour
 
 	public ParticleSystem vomitSystem;
 	private bool inVomit;
+	private bool hasStopVomit;
 
 	//debug
-	private int score = 50;
+	private int score = ScoreController.score;
 
 	// Use this for initialization
 	void Start ()
@@ -40,9 +44,10 @@ public class GameOverController : MonoBehaviour
 
 		fishCount = 0;
 		fishSpawnCount = 0f;
-		fishSpawnRate = score * 20 / 12;
+		fishSpawnRate = score / 4;
 
 		inVomit = false;
+		hasStopVomit = false;
 	}
 	
 
@@ -66,18 +71,20 @@ public class GameOverController : MonoBehaviour
 			SkinnyCat.SetActive (true);
 
 		} else if (timepassed == 15) {
+			
 			Score.SetActive (true);
+			ScoreText.text = "Score: " + score;
 
 		} else if (timepassed == 18) {
 			ReturnButton.SetActive (true);
 		}
 
 
-		if (timepassed >= 7 && !inVomit) {
+		if (timepassed >= 6 && !inVomit) {
 			vomitSystem.Emit (1);
 		}
 
-		if (timepassed >= 7 && fishCount < score) {
+		if (timepassed >= 6 && fishCount < score) {
 
 			if (fishSpawnCount < 0) {
 				spawnFish ();
@@ -85,13 +92,18 @@ public class GameOverController : MonoBehaviour
 			}
 			fishSpawnCount -= Time.deltaTime;
 		}
+
+		if (timepassed >= 12 && !hasStopVomit) {
+			vomitSystem.Pause ();
+			hasStopVomit = true;
+		}
 	}
 
 	private void spawnFish() {
+		
 		GameObject newFish;
 		newFish = Instantiate (fishPrefab, new Vector2 (fishStackSpawn.transform.position.x,
-			fishStackSpawn.transform.position.y + fishCount * 4), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
-		newFish.transform.localScale = new Vector3 (10, 10, 1);
+			fishStackSpawn.transform.position.y + fishCount * 4), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));		
 
 		fishCount++;
 
